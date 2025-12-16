754ca0cbe17498cc5e54194c8e6e3035551b94d3
Gliitchhh410 <ahmedmounir532@gmail.com>
2025-12-17 00:11:26 +0200

    feat: setting up example in main.c

--- main.cpp
+++ main.cpp
@@ -1,78 +1,59 @@
-
 #include <iostream>
-#include <cstring>
+#include "bank.h"
+
 using namespace std;
-
-void func(int &x) { x = 20; }
-void func(int x) { x = 30; }
-int *findChars(char *s, char c)
-{
-    int i = 0;
-    int j = 0;
-    int *res = new int[100];
-    while (s[i] != '\0')
-    {
-        if (tolower(s[i]) == c)
-        {
-            *(res + j) = i;
-            j++;
-        }
-        i++;
-    }
-    *(res + j) = -1;
-    return res;
-}
-
-int sum(int n)
-{
-    if (n == 1)
-        return 1;
-    return n + sum(n - 1);
-}
-
-int diagSum(int** arr, int n)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += arr[i][i];
-    }
-    return sum;
-}
-
-void concatStr(char *dest, char *source)
-{
-    int i = 0;
-    int j = 0;
-
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-    dest[i] = ' ';
-    i++;
-    while (source[j] != '\0')
-    {
-        dest[i] = source[j];
-        i++;
-        j++;
-    }
-
-    dest[i] = '\0';
-}
 
 int main()
 {
-    int n = 2;
-    int **arr = new int *[n];
+    // 1. Setup Bank
+    Bank myBank;
 
-    for (int i = 0; i < n; i++)
+    // 2. Create a Customer
+    myBank.createCustomer("Osama");
+    // We assume the ID generated is accessible or printed.
+    // For this test, let's assume the ID generated was handled internally or we just guess it's the first one.
+    // NOTE: In your code, createCustomer generates a random ID.
+    // You might want to print that ID so the user knows what it is!
+
+    int customerId = myBank.Customers[0].getCustomerId(); // Hack to get the ID for testing
+    cout << "Customer created with ID: " << customerId << endl;
+
+    // 3. Use the FACTORY PATTERN to create accounts
+
+    // Create a Savings Account
+    SavingsAccountFactory savingsFactory;
+    myBank.openAccount(&savingsFactory, customerId);
+
+    // Create a Checking Account
+    CheckingAccountFactory checkingFactory;
+    myBank.openAccount(&checkingFactory, customerId);
+
+    // 4. Retrieve the Customer pointer
+    Customer *osama = nullptr;
+    for (auto &c : myBank.Customers)
     {
-        arr[i] = new int[n];
+        if (c.getCustomerId() == customerId)
+            osama = &c;
     }
-    arr[0][0] = 1;
-    arr[0][1] = 2;
-    arr[1][0] = 1;
-    arr[1][1] = 2;
-    cout << diagSum(arr, 2);
+
+    // 5. Test Operations
+    if (osama)
+    {
+        // Get the first account (Savings)
+        Account *acc1 = osama->getAccount(0);
+        cout << "\n--- Testing Savings (Acc ID: " << acc1->getAccountNumber() << ") ---" << endl;
+        acc1->deposit(500);
+        acc1->withdraw(200);
+        acc1->withdraw(1000); // Should Fail
+
+        // Get the second account (Checking)
+        Account *acc2 = osama->getAccount(1);
+        cout << "\n--- Testing Checking (Acc ID: " << acc2->getAccountNumber() << ") ---" << endl;
+        acc2->deposit(100);
+        acc2->withdraw(400); // Should Work (Overdraft)
+
+        acc2->printStatement();
+    }
+
+    return 0;
 }
